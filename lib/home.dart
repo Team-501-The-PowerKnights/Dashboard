24859114fe508dbb4497aa3647fbfc38df1b2f08
Matthew Gleich <matthewgleich@gmail.com>
2020-03-07 10:05:45 -0500

    Update application

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 import 'package:scouting_dashboard/widgets/animations/animateInText.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
-          padding: const EdgeInsets.all(7.0),
+          padding: const EdgeInsets.only(
+            bottom: 7.0,
+            top: 7.0,
+            left: 10.0,
+            right: 10.0,
+          ),
           child: FloatingActionButton(
-            onPressed: null,
+            hoverColor: Colors.blue,
+            onPressed: () => print(""),
             child: Icon(
               Icons.settings,
             ),
@@ -21,14 +32,32 @@ class Home extends StatelessWidget {
         ),
         title: AnimateInText(
           duration: Duration(seconds: 1),
-          text:
-              "📊 Scouting Dashboard, ${DateFormat.yMMMd().format(DateTime.now())}",
+          text: "📊 Scouting Dashboard",
           verticalOffset: 50,
           horizontalOffset: 50,
           mainAxis: MainAxisAlignment.start,
           crossAxis: CrossAxisAlignment.start,
         ),
         backgroundColor: Colors.black,
+        actions: <Widget>[FloatingActionButton(onPressed: () {})],
+      ),
+      body: GridView.count(
+        crossAxisCount: 4,
+        children: List.generate(
+          100,
+          (int index) {
+            return AnimationConfiguration.staggeredGrid(
+              position: index,
+              duration: const Duration(seconds: 1),
+              columnCount: 4,
+              child: ScaleAnimation(
+                child: FadeInAnimation(
+                  child: Text("Hello"),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
